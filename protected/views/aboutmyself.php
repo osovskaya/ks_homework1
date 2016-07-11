12fca89932e07f7dea9a2dd6ae48d32a9f05b4c2
Tatyana Osovskaya <tanya.osovskaya@mail.ru>
2016-07-11 08:51:30 +0300

    Update aboutmyself.php

--- protected/views/aboutmyself.php
+++ protected/views/aboutmyself.php
@@ -2,20 +2,24 @@
 
 <h2>Information about myself</h2>
 
-<div class="table">
-    <table>
-        <?php foreach ($_SESSION['user'] as $key => $value): ?>
-        <tr>
-            <td class="key"><?php echo ucfirst($key); ?></td>
-            <td class="value"><?php echo $value; ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
-<div class="image">
-    <img src="<?php echo $_SESSION['userfiles']['img']; ?>" />
-</div>
-<h3>My story</h3>
-<p><?php echo $_SESSION['aboutmyself'];?></p>
+<?php if (empty($_SESSION['user'])): ?>
+<p>You haven't add information yet</p>
+<?php else: ?>
+	<div class="table">
+		<table>
+		    <?php foreach ($_SESSION['user'] as $key => $value): ?>
+		    <tr>
+		        <td class="key"><?php echo ucfirst($key); ?></td>
+		        <td class="value"><?php echo $value; ?></td>
+		    </tr>
+		    <?php endforeach; ?>
+		</table>
+	</div>
+	<div class="image">
+		<img src="<?php echo $_SESSION['userfiles']['img']; ?>" />
+	</div>
+	<h3>My story</h3>
+	<p><?php echo $_SESSION['aboutmyself'];?></p>
+<?php endif; ?>
 
 <?php require_once(__DIR__.'/layouts/footer.php');?>
